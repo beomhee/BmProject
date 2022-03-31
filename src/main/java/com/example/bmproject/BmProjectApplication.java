ad2475d41602d89bd95a2fea4f3d4a7403bd2097
beomhee <itsbeomy@gmail.com>
2022-03-31 15:49:27 +0900

    init

---
+++ src/main/java/com/example/bmproject/BmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.bmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bmproject/BmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bmproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
